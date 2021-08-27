4f8948b09f21bef4e147db199a4ecd9dfc34d99f
andres meira <andresmeira@gmail.com>
2021-08-26 19:30:40 -0500

    add s3 firmware update

--- AWS_IoT/src/main.cpp
+++ AWS_IoT/src/main.cpp
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
+#include <Update.h>
 #include <ETH.h>
 #include <time.h>
 #include <Preferences.h>
@@ -30,10 +31,24 @@ const char MQTT_TOPIC_SENDACCEL[] = "iot-2/cmd/sendacceldata/fmt/json";
 const char MQTT_TOPIC_RESTART[] = "iot-2/cmd/forcerestart/fmt/json";
 const char MQTT_TOPIC_THRESHOLD[] = "iot-2/cmd/threshold/fmt/json";
 const char MQTT_TOPIC_FACTORYRST[] = "iot-2/cmd/factoryreset/fmt/json";
+const char MQTT_TOPIC_FWUPDATE[] = "iot-2/cmd/firmwareupdate/fmt/json";
+
 char deviceID[13];
 
 WiFiClientSecure net;
 PubSubClient client(net);
+
+// OTA
+long contentLength = 0; // response from S3
+bool isValidContentType = false;
+
+String host = S3_FIRMWARE_BUCKET; // Host => bucket-name.s3.region.amazonaws.com
+int port = 80; // Non https. For HTTPS 443. As of today, HTTPS doesn't work.
+String bin = S3_FILE; // bin file name with a slash in front.
+
+String getHeaderValue(String header, String headerName) {
+  return header.substring(strlen(headerName.c_str()));
+}
 
 // Activation
 bool FirmwareVersionCheck( char *, String );
@@ -188,6 +203,133 @@ void StartADXL355() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 1); //enable brownout detector
 }
 
+// OTA Logic 
+void execOTA() {
+  Serial.println("Connecting to: " + String(host));
+  // Connect to S3
+  if (net.connect(host.c_str(), port)) {
+    // Connection Succeed.
+    // Fecthing the bin
+    Serial.println("Fetching Bin: " + String(bin));
+
+    // Get the contents of the bin file
+    client.print(String("GET ") + bin + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Cache-Control: no-cache\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    // Check what is being sent
+    //    Serial.print(String("GET ") + bin + " HTTP/1.1\r\n" +
+    //                 "Host: " + host + "\r\n" +
+    //                 "Cache-Control: no-cache\r\n" +
+    //                 "Connection: close\r\n\r\n");
+
+    unsigned long timeout = millis();
+    while (net.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println("Client Timeout !");
+        net.stop();
+        return;
+      }
+    }
+
+    while (net.available()) {
+      // read line till /n
+      String line = net.readStringUntil('\n');
+      // remove space, to check if the line is end of headers
+      line.trim();
+
+      // if the the line is empty,
+      // this is end of headers
+      // break the while and feed the
+      // remaining `client` to the
+      // Update.writeStream();
+      if (!line.length()) {
+        //headers ended
+        break; // and get the OTA started
+      }
+
+      // Check if the HTTP Response is 200
+      // else break and Exit Update
+      if (line.startsWith("HTTP/1.1")) {
+        if (line.indexOf("200") < 0) {
+          Serial.println("Got a non 200 status code from server. Exiting OTA Update.");
+          break;
+        }
+      }
+
+      // extract headers here
+      // Start with content length
+      if (line.startsWith("Content-Length: ")) {
+        contentLength = atol((getHeaderValue(line, "Content-Length: ")).c_str());
+        Serial.println("Got " + String(contentLength) + " bytes from server");
+      }
+
+      // Next, the content type
+      if (line.startsWith("Content-Type: ")) {
+        String contentType = getHeaderValue(line, "Content-Type: ");
+        Serial.println("Got " + contentType + " payload.");
+        if (contentType == "application/octet-stream") {
+          isValidContentType = true;
+        }
+      }
+    }
+  } else {
+    // Connect to S3 failed
+    // May be try?
+    // Probably a choppy network?
+    Serial.println("Connection to " + String(host) + " failed. Please check your setup");
+    // retry??
+    // execOTA();
+  }
+
+  // Check what is the contentLength and if content type is `application/octet-stream`
+  Serial.println("contentLength : " + String(contentLength) + ", isValidContentType : " + String(isValidContentType));
+
+  // check contentLength and content type
+  if (contentLength && isValidContentType) {
+    // Check if there is enough to OTA Update
+    bool canBegin = Update.begin(contentLength);
+
+    // If yes, begin
+    if (canBegin) {
+      Serial.println("Begin OTA. This may take 2 - 5 mins to complete. Things might be quite for a while.. Patience!");
+      // No activity would appear on the Serial monitor
+      // So be patient. This may take 2 - 5mins to complete
+      size_t written = Update.writeStream(net);
+
+      if (written == contentLength) {
+        Serial.println("Written : " + String(written) + " successfully");
+      } else {
+        Serial.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?" );
+        // retry??
+        // execOTA();
+      }
+
+      if (Update.end()) {
+        Serial.println("OTA done!");
+        if (Update.isFinished()) {
+          Serial.println("Update successfully completed. Rebooting.");
+          ESP.restart();
+        } else {
+          Serial.println("Update not finished? Something went wrong!");
+        }
+      } else {
+        Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+      }
+    } else {
+      // not enough space to begin OTA
+      // Understand the partitions and
+      // space availability
+      Serial.println("Not enough space to begin OTA");
+      net.flush();
+    }
+  } else {
+    Serial.println("There was no content in the response");
+    net.flush();
+  }
+}
+
 // Handle subscribed MQTT topics - Alerts and Sample Rate changes
 void callback(char* topic, byte* payload, unsigned int length) {
   StaticJsonDocument<100> jsonMQTTReceiveDoc;
@@ -224,6 +366,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
       NeoPixelStatus( LED_FIRMWARE_OTA ); // Firmware OTA - Magenta
       //OpenEEWDeviceActivation(); 
       //  Do Firmware Update Here
+    } else if ( strcmp(topic, MQTT_TOPIC_FWUPDATE) == 0 ) {
+      NeoPixelStatus( LED_FIRMWARE_OTA ); // Firmware OTA - Magenta
+      execOTA();
+      
     } else if ( strcmp(topic, MQTT_TOPIC_SEND10SEC) == 0 ) {
       // Send 10 seconds of accelerometer history
       Serial.println("Send 10 seconds of accelerometer history to the cloud");
@@ -602,6 +748,8 @@ void checkWiFiThenReboot(void)
   Serial.print("Rebooting");
   ESP.restart();
 }
+
+
 
 
 void setup() {
@@ -1213,3 +1361,4 @@ void AlarmBuzzer() {
   ledcWrite(channel, 4000);
   delay(100);
 }
+
